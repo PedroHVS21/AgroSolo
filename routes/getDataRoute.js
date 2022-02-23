490e86d9702e75561336d61b6bf686a18f2554e7
pedrosilva <pedrohenriquevianas@hotmail.com>
2022-02-23 12:16:51 -0100

    fix getdata route

--- routes/getDataRoute.js
+++ routes/getDataRoute.js
@@ -1,21 +1,30 @@
 const express = require('express')
+const res = require('express/lib/response')
 const router = express.Router()
 
 const exemploModel = require('../models/exemploModel')
 
-router.get('/', (req, res) => {
-    exemploModel.findOne(
-        {'idArduino':'123'})
+router.get('/:idArduino', (req, res) => {
+    exemploModel.findOne({'idArduino':{$eq: req.params.idArduino}})
     .exec()
-    .then((result) => {
-        console.log(result);
-        res.send(result)
-    })
-    .catch(error => {
-                res.json({
-                    msg: 'Ocorreu um erro'
-                })
+    .then((result)=>{
+        if(result != 0){
+            return res.json({ 
+                tempAr:result.tempAr,
+                humAr: result.humAr,
+                estadoSolo: result.estadoSolo
             })
+        }
+        return res.json({
+            msg:'não encontrado'
         })
+    })
+    .catch(error =>{
+        console.log(error)
+    })
+})
 
 module.exports = router
+
+
+
